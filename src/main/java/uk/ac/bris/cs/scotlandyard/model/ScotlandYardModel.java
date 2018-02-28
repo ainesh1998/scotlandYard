0884be5eee9eb613a21d8afe231f6c30b822ba82
michael <mm17291@bristol.ac.uk>
2018-02-28 20:03:03 +0000

    Constructor has been started.Some tests in ModelCreationTest now pass

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -1,19 +1,59 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.Objects.requireNonNull;
+import static uk.ac.bris.cs.scotlandyard.model.Colour.BLACK;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.DOUBLE;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.SECRET;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
+import java.util.function.Consumer;
+import uk.ac.bris.cs.gamekit.graph.Edge;
 import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
 
 // TODO implement all methods and pass all tests
 public class ScotlandYardModel implements ScotlandYardGame {
+	private List<Boolean> rounds;
+	private Graph<Integer,Transport> map;
+	private PlayerConfiguration mrX, Detective1;
+	private List<PlayerConfiguration> restOfDetectives;
 
+
+	private boolean isNotDetective(PlayerConfiguration x){ //checks whether a player is a detective or not
+	    return (x.colour.isMrX() || x.tickets.containsKey(DOUBLE)|| x.tickets.containsKey(SECRET));
+    }
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
 			PlayerConfiguration... restOfTheDetectives) {
-		// TODO
+		this.rounds = requireNonNull(rounds);
+		this.map = requireNonNull(graph);
+		this.mrX = requireNonNull(mrX);
+		if(graph.isEmpty()) //map cannot be empty
+		    throw new IllegalArgumentException("map should not be empty");
+
+		if(mrX.colour != BLACK) // mrX cannot be a detective's colour
+            throw new IllegalArgumentException("MrX should be Black");
+
+		this.Detective1 = requireNonNull(firstDetective);  //If there is a mrX in your list of detectives it should fail
+        if(isNotDetective(firstDetective))
+            throw new IllegalArgumentException("Only 1o mrX is allowed");
+
+		for( PlayerConfiguration x : restOfTheDetectives){
+            restOfDetectives.add(requireNonNull(x));
+            if(isNotDetective(x)) //If there is a mrX in your list of detectives it should fail
+		        throw new IllegalArgumentException("Only 1 mrX is allowed.");
+        }
 	}
 
 	@Override
