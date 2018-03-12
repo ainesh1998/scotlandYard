3fdc61125baa8b63320fff2e02223699afbe3c8a
michael <mm17291@bristol.ac.uk>
2018-03-12 18:16:58 +0000

    Only 1 test remaining in ModelPlayerTest

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -36,6 +36,7 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move>{
 	private int currentPlayer;
 	private boolean gameOver = false;
 	private int xLastLocation = 0;
+	private int checkZeroTwice = 0;
 
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 							 PlayerConfiguration mrX, PlayerConfiguration firstDetective,
@@ -219,9 +220,11 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move>{
 				currentRound += 1;
 			}
 			if(currentRound >= rounds.size()){
-                if(currentRound == 1 && p.isMrX()) //This just undoes the increment
+                if(currentRound == 1 ) { //This just undoes the increment
                     //Don't think it's efficient
                     currentRound = 0;
+                    checkZeroTwice += 1;
+                }
                 else
 				    gameOver = true;
 			}
@@ -265,7 +268,9 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move>{
 		for (ScotlandYardPlayer player : players) {
 		    if (player.colour() == colour) {
 		        if (player.isMrX()) {
-		            if (rounds.get(getCurrentRound()) && currentRound != 0) {
+		            if (rounds.get(getCurrentRound())) {
+		                if(currentRound == 0 && checkZeroTwice == 0)
+		                    return Optional.of(0);
 		            	xLastLocation = player.location();
 		            	return Optional.of(player.location());
 		            }
